75ef679138727ab1b114ad065affe93c11198631
Kostiantyn Lapshyn1 <kostiantyn_lapshyn1@epam.com>
2021-02-09 15:19:17 +0200

    Created Spring Boot application

---
+++ src/main/java/com/poohdev/booksharing/BookSharingApplication.java
@@ -0,0 +1,13 @@
+package com.poohdev.booksharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poohdev/booksharing/BookSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poohdev.booksharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
